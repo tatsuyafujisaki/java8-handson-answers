4d86d9b24aa7f4350ba5850684cfe962d4a8a49d
Tatsuya <tatsuya@users.noreply.github.com>
2016-08-28 03:02:51 +0900

    Update Sample07.java

--- src/Sample07.java
+++ src/Sample07.java
@@ -5,10 +5,10 @@ public class Sample07 {
     public static void main(String[] args) {
         List<Integer> games = Arrays.asList(118, 65, 152, 201, 126);
         int totalScore = games
-                .stream()
-                .filter(n -> 101 <= n && n < 200)
-                .reduce((a, b) -> a + b)
-                .get();
+                         .stream()
+                         .filter(n -> 101 <= n && n < 200)
+                         .reduce((a, b) -> a + b)
+                         .get();
 
         System.out.println(totalScore);
     }
